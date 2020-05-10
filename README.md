a14d3725d90dcc71b705fcc976d38e3e6872eeed
serach24 <jiangchenhao2@outlook.com>
2020-05-10 19:47:52 -0400

    migrate from svn to github and add README

---
+++ README.md
@@ -0,0 +1,16 @@
+# Lispy
+
+> Scheme Interpreter in Python
+
+This project is learned from Peter Norvig http://norvig.com/lispy.html
+
+### How to use
+
+``` 
+python3 lispy.py
+lispy>
+lispy> (define circle-area (lambda (r) (* pi (* r r))))
+lispy> (circle-area 1) 
+3.141592653589793
+lispy> 
+```
---
+++ lispy.py
@@ -0,0 +1,24 @@
+# Interaction
+
+from environment import List
+from eval import eval
+from parsing import parse
+
+
+def repl(prompt='lispy> '):
+    "A prompt-read-eval-print loop."
+    while True:
+        val = eval(parse(input(prompt)))
+        if val is not None:
+            print(lispstr(val))
+
+def lispstr(exp):
+    "Convert a Python object back into a Lisp-readable string."
+    if isinstance(exp, List):
+        return '(' + ' '.join(map(lispstr, exp)) + ')'
+    return str(exp)
+
+
+
+if __name__ == "__main__":
+    repl()
